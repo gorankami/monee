dedc950714b4a70f5ca297b0d2382c2c4f284f1b
Goran <gorankami@gmail.com>
2021-04-29 22:18:04 +0200

    converted export to module.exports

--- src/digest/convertPurposeCategoryMapToArray.js
+++ src/digest/convertPurposeCategoryMapToArray.js
@@ -1,6 +1,8 @@
-export function convertPurposeCategoryMapToArray(map) {
+function convertPurposeCategoryMapToArray(map) {
   return Object.keys(map).map((purpose) => ({
     purpose,
     category: map[purpose],
   }))
 }
+
+module.exports = convertPurposeCategoryMapToArray
